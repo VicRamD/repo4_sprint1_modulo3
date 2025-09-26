dee976be828f9b4065e6dc6d77b5a2755dc1a7e1
Víctor Francisco Ramírez Díaz <franvictor18@gmail.com>
2025-09-25 21:59:24 -0300

    añadiendo logica del servicio y la persistencia

---
+++ repository/superheroesDataSource.mjs
@@ -0,0 +1,7 @@
+
+export default class SuperheroesDataSource{
+    //metodo abstracto para obtener todos los superheroes
+    obtenerTodos() {
+        throw new Error('Este método debe ser implementado por la subclase');
+    }
+}
